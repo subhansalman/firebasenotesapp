3dd4e51efc83451348231a2cd3b33b1bd7811f5c
subhansalman <subhansalman492@gmail.com>
2025-01-12 21:57:02 +0500

    Suignup form initialzed

--- signup.js
+++ signup.js
@@ -1,2 +1,7 @@
 import{ auth, createUserWithEmailAndPassword} from "./firebase.js"
 
+const email=document.querySelector("#email-signup")
+const password= document.querySelector("#email-password")
+const userName= document.querySelector("#username")
+const userPhone= document.querySelector("#phone")
+const userbirthdate= document.querySelector("#date") 
