c348be13cf43ddc34e10266211ab39fdbcfd453b
subhansalman <subhansalman492@gmail.com>
2025-01-13 05:33:13 +0500

    checking a conditon

--- todo.js
+++ todo.js
@@ -1,4 +1,12 @@
+const authCheck=()=>{
+    const userUID= localStorage.getItem("uid")
+    if(userUID){
+        window.location.replace("./index.html")
+    }
+}
+
 import {app, collection, addDoc, getDocs, doc, updateDoc, db} from "./firebase.js"
+
 console.log(app)
 const inputNotes=document.querySelector("#inputvalue");
 const parentNotes=document.querySelector("#parent");
@@ -28,14 +36,17 @@ const fetchData= async ()=>{
     parentNotes.innerHTML="";
     gettingQuery.forEach((doc)=>{
         console.log(doc.data())
-parentNotes.innerHTML += `<div class="col-sm-6 col-md-4 col-lg-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">${doc.data().todo}</h5>
-        <button class="btn btn-success" onclick="updateNotes(this)" id="${doc.id}">Edit Notes</button>
-      </div>
-    </div>
-  </div>`
+        if(doc.data().uid === localStorage.getItem("uid")){
+
+            parentNotes.innerHTML += `<div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+            <div class="card-body">
+            <h5 class="card-title">${doc.data().todo}</h5>
+            <button class="btn btn-success" onclick="updateNotes(this)" id="${doc.id}">Edit Notes</button>
+            </div>
+            </div>
+            </div>`
+            }
     })
     } catch (error) {
         console.log("error",error.message)
@@ -63,3 +74,4 @@ const updateNotes= async(ele)=>{
 window.addData=addData
 window.fetchData = fetchData
 window.updateNotes=updateNotes
+window.authCheck=authCheck
